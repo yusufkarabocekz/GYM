df6c67484f788d137a92bcb9ca2c48916a08d452
yusufkarabocekz <yusufkarabocekz@gmail.com>
2023-03-13 19:47:51 +0300

    the application that makes gym membership pay vs transactions

---
+++ GYMApplication/GuncelleSil.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace GYMApplication
+{
+    partial class GuncelleSil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label8 = new System.Windows.Forms.Label();
+            this.anamenubutton = new System.Windows.Forms.Button();
+            this.deletebutton = new System.Windows.Forms.Button();
+            this.resetbutton = new System.Windows.Forms.Button();
+            this.updatebutton = new System.Windows.Forms.Button();
+            this.timingcombo = new System.Windows.Forms.ComboBox();
+            this.cinsiyetcombo = new System.Windows.Forms.ComboBox();
+            this.amounttext = new System.Windows.Forms.TextBox();
+            this.yastext = new System.Windows.Forms.TextBox();
+            this.phonetext = new System.Windows.Forms.TextBox();
+            this.nametext = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.form2exit = new System.Windows.Forms.Button();
+            this.memberlist2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.memberlist2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(490, 100);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(309, 22);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Silmek için kullanıcıya tıklayınız.";
+            // 
+            // anamenubutton
+            // 
+            this.anamenubutton.Location = new System.Drawing.Point(211, 448);
+            this.anamenubutton.Name = "anamenubutton";
+            this.anamenubutton.Size = new System.Drawing.Size(126, 55);
+            this.anamenubutton.TabIndex = 48;
+            this.anamenubutton.Text = "ANA MENÜ";
+            this.anamenubutton.UseVisualStyleBackColor = true;
+            this.anamenubutton.Click += new System.EventHandler(this.anamenubutton_Click);
+            // 
+            // deletebutton
+            // 
+            this.deletebutton.Location = new System.Drawing.Point(211, 346);
+            this.deletebutton.Name = "deletebutton";
+            this.deletebutton.Size = new System.Drawing.Size(126, 55);
+            this.deletebutton.TabIndex = 47;
+            this.deletebutton.Text = "SIL";
+            this.deletebutton.UseVisualStyleBackColor = true;
+            this.deletebutton.Click += new System.EventHandler(this.deletebutton_Click);
+            // 
+            // resetbutton
+            // 
+            this.resetbutton.Location = new System.Drawing.Point(211, 230);
+            this.resetbutton.Name = "resetbutton";
+            this.resetbutton.Size = new System.Drawing.Size(126, 55);
+            this.resetbutton.TabIndex = 46;
+            this.resetbutton.Text = "SIFIRLA";
+            this.resetbutton.UseVisualStyleBackColor = true;
+            this.resetbutton.Click += new System.EventHandler(this.resetbutton_Click);
+            // 
+            // updatebutton
+            // 
+            this.updatebutton.Location = new System.Drawing.Point(212, 128);
+            this.updatebutton.Name = "updatebutton";
+            this.updatebutton.Size = new System.Drawing.Size(126, 55);
+            this.updatebutton.TabIndex = 45;
+            this.updatebutton.Text = "GÜNCELLE";
+            this.updatebutton.UseVisualStyleBackColor = true;
+            this.updatebutton.Click += new System.EventHandler(this.updatebutton_Click);
+            // 
+            // timingcombo
+            // 
+            this.timingcombo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.timingcombo.FormattingEnabled = true;
+            this.timingcombo.Items.AddRange(new object[] {
+            "1 AY",
+            "2 AY",
+            "3 AY",
+            "4 AY",
+            "5 AY",
+            "6 AY",
+            "7 AY",
+            "8 AY",
+            "9 AY",
+            "10 AY",
+            "11 AY",
+            "12 AY"});
+            this.timingcombo.Location = new System.Drawing.Point(15, 473);
+            this.timingcombo.Name = "timingcombo";
+            this.timingcombo.Size = new System.Drawing.Size(154, 30);
+            this.timingcombo.TabIndex = 44;
+            // 
+            // cinsiyetcombo
+            // 
+            this.cinsiyetcombo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.cinsiyetcombo.FormattingEnabled = true;
+            this.cinsiyetcombo.Items.AddRange(new object[] {
+            "KADIN",
+            "ERKEK"});
+            this.cinsiyetcombo.Location = new System.Drawing.Point(15, 277);
+            this.cinsiyetcombo.Name = "cinsiyetcombo";
+            this.cinsiyetcombo.Size = new System.Drawing.Size(154, 30);
+            this.cinsiyetcombo.TabIndex = 43;
+            // 
+            // amounttext
+            // 
+            this.amounttext.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.amounttext.Location = new System.Drawing.Point(15, 404);
+            this.amounttext.Multiline = true;
+            this.amounttext.Name = "amounttext";
+            this.amounttext.Size = new System.Drawing.Size(154, 41);
+            this.amounttext.TabIndex = 42;
+            // 
+            // yastext
+            // 
+            this.yastext.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.yastext.Location = new System.Drawing.Point(15, 335);
+            this.yastext.Multiline = true;
+            this.yastext.Name = "yastext";
+            this.yastext.Size = new System.Drawing.Size(154, 41);
+            this.yastext.TabIndex = 41;
+            // 
+            // phonetext
+            // 
+            this.phonetext.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.phonetext.Location = new System.Drawing.Point(15, 200);
+            this.phonetext.Multiline = true;
+            this.phonetext.Name = "phonetext";
+            this.phonetext.Size = new System.Drawing.Size(154, 41);
+            this.phonetext.TabIndex = 40;
+            // 
+            // nametext
+            // 
+            this.nametext.Location = new System.Drawing.Point(15, 128);
+            this.nametext.Multiline = true;
+            this.nametext.Name = "nametext";
+            this.nametext.Size = new System.Drawing.Size(154, 41);
+            this.nametext.TabIndex = 39;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 448);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 22);
+            this.label7.TabIndex = 38;
+            this.label7.Text = "Süre";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 22);
+            this.label6.TabIndex = 37;
+            this.label6.Text = "Cinsiyet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 379);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 22);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "Aylık Ücret";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 22);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Yas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 22);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "Telefon Numarası";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 22);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Üye Adı";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Teal;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 16F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-2, -6);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(947, 98);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "KARABÖCEK SPOR SALONU\r\nGüncelleme ve Silme İşlemleri";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // form2exit
+            // 
+            this.form2exit.BackColor = System.Drawing.Color.Black;
+            this.form2exit.Font = new System.Drawing.Font("Rockwell", 12F);
+            this.form2exit.ForeColor = System.Drawing.Color.DarkOrange;
+            this.form2exit.Location = new System.Drawing.Point(875, 8);
+            this.form2exit.Name = "form2exit";
+            this.form2exit.Size = new System.Drawing.Size(50, 50);
+            this.form2exit.TabIndex = 51;
+            this.form2exit.Text = "X";
+            this.form2exit.UseVisualStyleBackColor = false;
+            this.form2exit.Click += new System.EventHandler(this.form2exit_Click);
+            // 
+            // memberlist2
+            // 
+            this.memberlist2.BackgroundColor = System.Drawing.Color.White;
+            this.memberlist2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Rockwell", 12F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Teal;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
+            this.memberlist2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.memberlist2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Rockwell", 12F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Teal;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.memberlist2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.memberlist2.Location = new System.Drawing.Point(363, 128);
+            this.memberlist2.Name = "memberlist2";
+            this.memberlist2.RowHeadersWidth = 51;
+            this.memberlist2.RowTemplate.Height = 24;
+            this.memberlist2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.memberlist2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.memberlist2.Size = new System.Drawing.Size(562, 375);
+            this.memberlist2.TabIndex = 52;
+            this.memberlist2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.memberlist2_CellClick);
+            this.memberlist2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.memberlist2_CellContentClick);
+            // 
+            // GuncelleSil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 527);
+            this.Controls.Add(this.memberlist2);
+            this.Controls.Add(this.form2exit);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.anamenubutton);
+            this.Controls.Add(this.deletebutton);
+            this.Controls.Add(this.resetbutton);
+            this.Controls.Add(this.updatebutton);
+            this.Controls.Add(this.timingcombo);
+            this.Controls.Add(this.cinsiyetcombo);
+            this.Controls.Add(this.amounttext);
+            this.Controls.Add(this.yastext);
+            this.Controls.Add(this.phonetext);
+            this.Controls.Add(this.nametext);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Rockwell", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "GuncelleSil";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SilForm";
+            this.Load += new System.EventHandler(this.GuncelleSil_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.memberlist2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button anamenubutton;
+        private System.Windows.Forms.Button deletebutton;
+        private System.Windows.Forms.Button resetbutton;
+        private System.Windows.Forms.Button updatebutton;
+        private System.Windows.Forms.ComboBox timingcombo;
+        private System.Windows.Forms.ComboBox cinsiyetcombo;
+        private System.Windows.Forms.TextBox amounttext;
+        private System.Windows.Forms.TextBox yastext;
+        private System.Windows.Forms.TextBox phonetext;
+        private System.Windows.Forms.TextBox nametext;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button form2exit;
+        private System.Windows.Forms.DataGridView memberlist2;
+    }
+}
---
+++ GYMApplication/OdemeForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace GYMApplication
+{
+    public partial class OdemeForm : Form
+    {
+        public OdemeForm()
+        {
+            InitializeComponent();
+        }
+
+
+        SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\visualtbl\GYMTBL.mdf;Integrated Security=True;Connect Timeout=30");
+
+        private void FillName()
+        {
+            sqlcon.Open();
+            SqlCommand cmd = new SqlCommand("Select MName from MemberTbl", sqlcon);
+            SqlDataReader rdr;
+            rdr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("MName", typeof(string));
+            dt.Load(rdr);
+            comboBox2.ValueMember = "MName";
+            comboBox2.DataSource = dt;
+
+            sqlcon.Close();
+        }
+
+        private void populasyon()
+        {
+            sqlcon.Open();
+            string sorgulama = "select * from PaymentTbl";
+            SqlDataAdapter sda = new SqlDataAdapter(sorgulama, sqlcon);
+            SqlCommandBuilder builder = new SqlCommandBuilder();
+            var ds = new DataSet();
+            sda.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            sqlcon.Close();
+
+        }
+
+        private void resetbutton_Click(object sender, EventArgs e)
+        {
+            
+            textBox2.Text = "";
+        }
+
+        private void geri_Click(object sender, EventArgs e)
+        {
+            Form2 anamenu = new Form2();
+            anamenu.Show();
+            this.Hide();
+        }
+        int key = 1;
+        private void pay_Click(object sender, EventArgs e)
+        {
+            if(comboBox2.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Eksik bilgi girdiniz.");
+            }
+            else
+            {
+                string payperiode = dateTimePicker1.Value.ToString() + dateTimePicker1.Value.Year.ToString();
+                sqlcon.Open();
+                SqlDataAdapter sda = new SqlDataAdapter("select count(*) from PaymentTbl where PMember='"+comboBox2.SelectedValue.ToString()+"' and PMonth='"+payperiode+"'",sqlcon);
+                DataTable dt = new DataTable();
+
+                sda.Fill(dt);
+
+                if (dt.Rows[0][0].ToString() == "1")
+                {
+                    MessageBox.Show("Bu ay için ödendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    string query = "insert into PaymentTbl values ('" + payperiode + "','" + comboBox2.SelectedValue.ToString()+"',"+textBox2.Text+")";
+                    SqlCommand cmd = new SqlCommand(query, sqlcon);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Ödeme işlemi başarıyla gerçekleşti.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                sqlcon.Close();
+            }
+
+            populasyon();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void OdemeForm_Load(object sender, EventArgs e)
+        {
+            FillName();
+            populasyon();
+        }
+    }
+}
